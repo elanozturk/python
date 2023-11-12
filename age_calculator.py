b372a5dcc66ac03e46ca5f63411c35e601948654
elanozturk <serkanoguzozturk@gmail.com>
2023-11-12 22:20:12 +0300

    Add files via upload

---
+++ age_calculator.py
@@ -0,0 +1,6 @@
+from datetime import date
+birthdate = int(input("Hangi yılda doğdunuz?"))
+def age(birthdate):
+    today = date.today()
+    return today.year - birthdate
+print("Yaşınız", age(birthdate))
