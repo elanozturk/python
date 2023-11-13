c0715546c23786e3f7e6deb0f21d78311d69ada1
elanozturk <serkanoguzozturk@gmail.com>
2023-11-13 21:24:37 +0300

    declare var a

--- age_calculator.py
+++ age_calculator.py
@@ -1,6 +1,8 @@
 from datetime import date
-birthdate = int(input("Hangi yılda doğdunuz?"))
+birthdate = int(input("Hangi yilda doğdunuz?"))
 def age(birthdate):
     today = date.today()
     return today.year - birthdate
-print("Yaşınız", age(birthdate))
+print("Yasiniz", age(birthdate))
+a=1
+
