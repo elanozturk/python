c97a06eb553fb9ea4861efca7d1f81a5b62b1ce1
elanozturk <serkanoguzozturk@gmail.com>
2023-11-16 23:24:45 +0300

    fix

--- age_calculator.py
+++ age_calculator.py
@@ -3,5 +3,6 @@ birthdate = int(input("Hangi yilda doğdunuz?"))
 def age(birthdate):
     today = date.today()
     return today.year - birthdate
-print("Yasiniz", age(birthdate))
+print("Yasiniz", age(birthdate)) #explain this line
+
 
