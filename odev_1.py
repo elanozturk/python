9720052db6f385a6f55b5cde72b3b746c708642f
elanozturk <serkanoguzozturk@gmail.com>
2023-11-27 15:16:01 +0300

    init

--- odev_1.py
+++ odev_1.py
@@ -26,3 +26,29 @@ if kullaniciadi == login_name:
 else:
     print("Kullanıcı adı bulunamadı.")
     print(".........................")
+
+
+# Comment all lines wit CTRL+C and K.
+# De-comment all lines with Ctrl+K AND U.
+
+# #import streamlit as st 
+# # def main():
+# #     kullaniciadi = "yönetici"
+# #     sifre = "1903"
+
+# #     login_name = st.text_input("Kullanıcı adı:")
+
+# #     if kullaniciadi == login_name:
+# #         st.success("Kullanıcı adı doğrulandı.")
+# #         login_pass = st.text_input("Parola:", type="password")
+
+# #         if sifre == login_pass:
+# #             st.success("Parola doğru, giriş başarılı.")
+# #         else:
+# #             st.error("Parola hatalı!")
+# #     else:
+# #         st.warning("Kullanıcı adı bulunamadı.")
+
+# # if __name__ == "__main__":
+# #     main()
+
