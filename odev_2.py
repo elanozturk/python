287569726ac06389648f1c22d2c2dd916a90565a
elanozturk <serkanoguzozturk@gmail.com>
2023-11-27 15:37:25 +0300

    init

--- odev_2.py
+++ odev_2.py
@@ -1,10 +1,22 @@
 # ÖDEV 2 (BURDA YANİ Google Colab'da yapalım, sonra streamlitte çalışan versiyonunu da yapalım)
 
-# ogrenciler=["Ali","Fatma","Mehmet","Çağrı"]
+ogrenciler = ["Ali","Fatma","Mehmet","Çağrı"]
 
-# ogrenci=input("Öğrenci Adı Giriniz")
+ogrenci = input("Öğrenci Adı Giriniz: ")
+
+ogrenci = ogrenci.title()
+
+if ogrenci in ogrenciler:
+    print(ogrenci, "listede zaten mevcut. Başka bir isim ekleyiniz.")
+else:
+     ogrenciler.append(ogrenci)
+
+     print(ogrenci, "listeye eklendi.")
+
+     print(ogrenciler)
 
 
 # öğrenci yoksa ekle ve bilgi ver
 # öğrenci varsa ekleme ve öğrenci zaten var diye bilgi ver
 # en son öğrencileri yazdır
+
