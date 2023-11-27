009e08ccefd5de604af74bced985c4ea44ff23ba
elanozturk <serkanoguzozturk@gmail.com>
2023-11-27 15:17:55 +0300

    init

---
+++ odev_2.py
@@ -0,0 +1,10 @@
+# ÖDEV 2 (BURDA YANİ Google Colab'da yapalım, sonra streamlitte çalışan versiyonunu da yapalım)
+
+# ogrenciler=["Ali","Fatma","Mehmet","Çağrı"]
+
+# ogrenci=input("Öğrenci Adı Giriniz")
+
+
+# öğrenci yoksa ekle ve bilgi ver
+# öğrenci varsa ekleme ve öğrenci zaten var diye bilgi ver
+# en son öğrencileri yazdır
