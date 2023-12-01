afb738d12eddec6e11daa6c72a0893a9c2fd6bf3
elanozturk <serkanoguzozturk@gmail.com>
2023-12-01 13:41:05 +0300

    init

--- Ari_Bilim/Odevler/Streamlit/pages/odev_1_streamlit_v2.py
+++ Ari_Bilim/Odevler/Streamlit/pages/odev_1_streamlit_v2.py
@@ -3,13 +3,13 @@ import yaml
 from yaml import SafeLoader
 import streamlit_authenticator as stauth
 
-hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
-
 with open("credentials.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+
 authenticator = stauth.Authenticate(
-    config['credentials'],
+    config.get('credentials'),
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
@@ -21,13 +21,16 @@ name, authentication_status, username = authenticator.login('Giriş', 'main')
 if authentication_status:
     authenticator.logout('Çıkış', 'main')
     st.write(f'Hoşgeldiniz *{name}*')
-    st.title('Some content')
-
-    # Yönlendirme için success veya write fonksiyonlarını kullanabilirsiniz
-    st.write('[Giriş sayfasına yönlendiriliyorsunuz](https://www.python.org/)')
-    st.redirect("https://www.python.org")
+    st.title('Giriş')
 
 elif authentication_status is False:
     st.error("Kullanıcı adı ve şifre hatalı.")
 elif authentication_status is None:
+    pass  # Do nothing if authentication_status is None
+
+elif authentication_status is True:
+    st.success("Giriş başarılı.")
+    #st.write("Giriş başarılı")
+
+else:
     st.warning('Lütfen kullanıcı adı ve şifrenizi giriniz.')
