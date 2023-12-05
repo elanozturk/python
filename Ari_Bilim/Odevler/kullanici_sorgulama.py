811ae577a28b69d73db99fccd72cec04cdac9709
elanozturk <serkanoguzozturk@gmail.com>
2023-12-05 15:25:46 +0300

    init

--- Ari_Bilim/Odevler/kullanici_sorgulama.py
+++ Ari_Bilim/Odevler/kullanici_sorgulama.py
@@ -1,11 +1,9 @@
-import streamlit as st
-
 users = {"admin": "1234", "fatih": "1453", "selanik": "1881", "dolmabahce": "1903"}
-login_request = st.text_input("Kullanıcı adını giriniz: ")
+login_request = input("Kullanıcı adını giriniz: ")
 
 if login_request in users.keys():
-    st.text_input_password = st.text_input("Kullanıcı adı tanımlı. Lütfen şifrenizi giriniz: ")
-    if st.text_input_password == users[login_request]:
+    input_password = input("Kullanıcı adı tanımlı. Lütfen şifrenizi giriniz: ")
+    if input_password == users[login_request]:
         print("Giriş başarılı.")
     else:
         print("Şifre yanlış")
