648732294853cf27d7597e8693bc14138e20c318
elanozturk <serkanoguzozturk@gmail.com>
2023-12-13 22:00:03 +0300

    init

---
+++ Various/def.py
@@ -0,0 +1,13 @@
+def greet(lang):
+   if lang == "es":
+      print ("Hola")
+   elif lang == "tr":
+      print ("Merhaba")
+   else:
+       print("Hello")
+       
+lang = input("Input language prefix: ")
+greet(lang)
+    
+  
+  
