17d284848ec73c3e8abc301a187eea97bab6fd13
elanozturk <serkanoguzozturk@gmail.com>
2023-12-17 13:21:40 +0300

    init

---
+++ arduino/sketches/probe.ino/probe.ino.cpp
@@ -0,0 +1,6 @@
+#define LED     D4        // Led in NodeMCU at pin GPIO16 (D0).
+ 
+#define BRIGHT    350     //max led intensity (1-500)
+#define INHALE    1250    //Inhalation time in milliseconds.
+#define PULSE     INHALE*1000/BRIGHT
+#define REST      1000    //Rest Between Inhalations.
